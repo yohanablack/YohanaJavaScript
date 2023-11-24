50660851f3230d37eaa7dc6cf0034262ed4ed9af
yohana <117214321+yohanablack@users.noreply.github.com>
2023-11-24 08:43:24 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,5 +2,17 @@ function tocaSomPom () {
    document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelectorAll ('.tecla')
+const listaDeTeclas = document.querySelectorAll ('.tecla');
+
+let contador;
+
+//enquanto
+while (contador < listaDeTeclas.length) {
+
+  listaDeTeclas[contador].onclick = tocaSomPom;
+
+  contador = contador + 1;
+
+  console.log(contador);
+}
 
